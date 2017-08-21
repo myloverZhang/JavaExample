5b6bc17503c6c2bfca3d3e601569aad6824e1270
myloverZhang <zhangzhengdi@outlook.com>
2017-08-21 16:42:16 +0800

    Add files via upload
    第一次提交

---
+++ springinitializr/src/test/java/com/example/demo/SpringinitializrApplicationTests.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringinitializrApplicationTests {
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
